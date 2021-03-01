aca51ec19c05d3f20ed6e7d54c59423d39a46124
hgs-study <hgstudy_@naver.com>
2021-03-01 16:37:30 +0900

    create enumTest project

---
+++ src/main/java/com/example/enum_test/EnumTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.enum_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnumTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnumTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/enum_test/EnumTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enum_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnumTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
